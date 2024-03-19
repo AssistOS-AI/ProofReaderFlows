872397838cbb3fefd90034bb48409a01931b100b
alupoaiemircea <mircea@axiologic.net>
2024-03-19 13:40:37 +0200

    system refactor

--- Proofread#U5JQR4MRtzrP.js
+++ Proofread#U5JQR4MRtzrP.js
@@ -27,7 +27,7 @@ export class Proofread {
 
     start(text, personalityId, prompt) {
         if (personalityId) {
-            let personality = webSkel.currentUser.space.getPersonality(personalityId);
+            let personality = system.space.getPersonality(personalityId);
             this.prompt = `Please suggest an improved version of the following text: "${text}". ${prompt || "Improve it both semantically and grammatically"}. You will play the role of this personality: "${personality.name}", which has the following characteristics: "${personality.description}". You will make observations about the writing style of the given text and provide an improved version of the given text by playing the role of the given personality. The response should have the following structure: {\"observations\": \"observations about the given text\", \"improvedText\": \"the improved version of the given text\"}.`;
         } else {
             this.prompt = `Please suggest an improved version of the following text: "${text}". ${prompt || "Improve it both semantically and grammatically"}. You will make observations about the writing style of the given text and provide an improved version of the given text. The response should have the following structure: {\"observations\": \"observations about the given text\", \"improvedText\": \"the improved version of the given text\"}.`;
