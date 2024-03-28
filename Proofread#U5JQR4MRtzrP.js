4f49f0836f4615871d4b0bc06ddc13210306da79
alupoaiemircea <mircea@axiologic.net>
2024-03-28 13:18:50 +0200

    update #44

--- Proofread#U5JQR4MRtzrP.js
+++ Proofread#U5JQR4MRtzrP.js
@@ -4,9 +4,7 @@ export class Proofread {
 
     static inputSchema=
         {
-            text:"string",
-            personalityId:"string",
-            prompt:"string"
+            text:"string"
         };
     async start(context, personality) {
         this.prompt = `Please suggest an improved version of the following text: "${context.text}". ${context.prompt || "Improve it both semantically and grammatically"}. You will play the role of this personality: "${personality.name}", which has the following characteristics: "${personality.description}". You will make observations about the writing style of the given text and provide an improved version of the given text by playing the role of the given personality. The response should have the following structure: {\"observations\": \"observations about the given text\", \"improvedText\": \"the improved version of the given text\"}.`;
